2c0821bf5b2beab8bcb30b8f64462950d86a2d6f
charlene-johnson <charlene.j8234@gmail.com>
2020-05-01 21:18:04 -0400

    Added a characterinfo  jss and worked through it, reached MVP

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,55 @@
 // Write your Character component here
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+import CharacterInfo from "./CharacterInfo"
+
+import styled from "styled-components"
+
+
+
+const UL = styled.ul `
+list-style-type:none;
+display: flex;
+flex-wrap: wrap;
+
+`
+
+
+export default function Characters() {
+    const[characters,setCharacters] = useState([])
+
+    useEffect(()=> {
+        axios
+            .get("https://swapi.py4e.com/api/people/")
+            .then(response => {
+                console.log(response.data.results)
+                setCharacters(response.data.results)
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, [])
+
+    return (
+        <div className="characterInfo">
+            <UL>
+            {characters.map(character => {
+                return (
+                    <CharacterInfo
+                        characterName={character.name}
+                        height={character.height}
+                        weight={character.mass}
+                        hairColor={character.hair_color}
+                        skinColor={character.skin_color}
+                        eyeColor={character.eye_color}
+                        birthday={character.birth_year}
+                        gender={character.gender}
+                      
+                    />
+                );
+            })}
+           </UL>
+        </div>
+    );
+}
